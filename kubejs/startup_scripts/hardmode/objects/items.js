75849f3332678cddd10b0f616be17c2f9dd42679
trulyno <tenodjikotaru04@gmail.com>
2026-06-01 10:56:30 +0300

    apply snake case to all names

--- kubejs/startup_scripts/hardmode/objects/items.js
+++ kubejs/startup_scripts/hardmode/objects/items.js
@@ -2,19 +2,35 @@
 
 StartupEvents.registry('item', (event) => {
     // === Primitive Age ===
-    event.create('flint_shard').rarity('common').texture(`kubejs:item/hm/pre-lv/flint_shard`);
+	event.create('flint_shard')
+		.rarity('common')
+		.texture(`kubejs:item/hm/pre_lv/flint_shard`);
 
-    event.create('plant_fibers').rarity('common').texture(`kubejs:item/hm/pre-lv/plant_fibers`);
+	event.create('plant_fibers')
+		.rarity('common')
+		.texture(`kubejs:item/hm/pre_lv/plant_fibers`);
 
-    event.create('packed_mud_ball').rarity('common').texture(`kubejs:item/hm/pre-lv/packed_mud_ball`);
+	event.create('packed_mud_ball')
+		.rarity('common')
+		.texture(`kubejs:item/hm/pre_lv/packed_mud_ball`);
 
-    event.create('mud_brick').rarity('common').texture(`kubejs:item/hm/pre-lv/packed_mud_brick`);
+	event.create('mud_brick')
+		.rarity('common')
+		.texture(`kubejs:item/hm/pre_lv/packed_mud_brick`);
 
-    event.create('stone_brick').rarity('common').texture(`kubejs:item/hm/pre-lv/stone_brick`);
+	event.create('stone_brick')
+		.rarity('common')
+		.texture(`kubejs:item/hm/pre_lv/stone_brick`);
 
-    event.create('charcoal_pellet').rarity('common').burnTime(720).texture(`kubejs:item/hm/pre-lv/charcoal_pellet`);
+	event.create('charcoal_pellet')
+		.rarity('common')
+		.burnTime(720)
+		.texture(`kubejs:item/hm/pre_lv/charcoal_pellet`);
 
-    event.create('water_bowl').rarity('common').unstackable().texture(`kubejs:item/hm/pre-lv/water_bowl`);
+	event.create('water_bowl')
+		.rarity('common')
+		.unstackable()
+		.texture(`kubejs:item/hm/pre_lv/water_bowl`);
 
     // === Ore Chunks ===
     [
@@ -29,8 +45,11 @@ StartupEvents.registry('item', (event) => {
         const { type, composition } = ore;
         let id = `${type}_crushed_ore_chunk`;
 
-        event.create(id).tooltip(`§e${composition}`).texture(`kubejs:item/hm/pre-lv/${id}`);
-    });
+		event.create(id)
+			.tooltip(`§e${composition}`)
+			.texture(`kubejs:item/hm/pre_lv/${id}`)
+	});
+
 
     // === Incomplete parts ===
     [
@@ -50,35 +69,31 @@ StartupEvents.registry('item', (event) => {
     });
 
     // === Tools ===
-    event
-        .create('basic_scavenging_rod')
-        .rarity('common')
-        .maxDamage(256)
-        .unstackable()
-        .texture(`kubejs:item/hm/pre-lv/basic_scavenging_rod`);
+	event.create('basic_scavenging_rod')
+		.rarity('common')
+		.maxDamage(256)
+		.unstackable()
+		.texture(`kubejs:item/hm/pre_lv/basic_scavenging_rod`);
 
-    event
-        .create('scavenging_rod')
-        .rarity('common')
-        .maxDamage(512)
-        .unstackable()
-        .texture(`kubejs:item/hm/pre-lv/scavenging_rod`);
+	event.create('scavenging_rod')
+		.rarity('common')
+		.maxDamage(512)
+		.unstackable()
+		.texture(`kubejs:item/hm/pre_lv/scavenging_rod`);
 
-    // === ULV Parts ===
-    ['electric_motor', 'electric_pump', 'conveyor_module', 'robot_arm', 'electric_piston', 'emitter'].forEach(
-        (type) => {
-            event.create(`ulv_${type}`).texture(`kubejs:item/hm/pre-lv/ulv_${type}`);
-        }
-    );
+	// === ULV Parts ===
+	['electric_motor', 'electric_pump', 'conveyor_module', 'robot_arm', 'electric_piston', 'emitter'].forEach(type => {
+		event.create(`ulv_${type}`)
+			.texture(`kubejs:item/hm/pre_lv/ulv_${type}`);
+	});
 
     // === Ceramic Molds
-    ['ingot', 'ball', 'raw'].forEach((ceramic) => {
-        event
-            .create(`unfired_${ceramic}_ceramic_casting_mold`)
-            .texture(`kubejs:item/hm/pre-lv/unfired_${ceramic}_ceramic_casting_mold`);
-        if (ceramic !== 'raw')
-            event
-                .create(`${ceramic}_ceramic_casting_mold`)
-                .texture(`kubejs:item/hm/pre-lv/${ceramic}_ceramic_casting_mold`);
-    });
+	['ingot', 'ball', 'raw'].forEach(ceramic => {
+		event.create(`unfired_${ceramic}_ceramic_casting_mold`)
+			.texture(`kubejs:item/hm/pre_lv/unfired_${ceramic}_ceramic_casting_mold`);
+		if (ceramic !== 'raw')
+		event.create(`${ceramic}_ceramic_casting_mold`)
+			.texture(`kubejs:item/hm/pre_lv/${ceramic}_ceramic_casting_mold`);
+	});
+
 });
