cd2b9150fceb6149f659a5ea50623b3d09f1f232
trulyno <78795220+trulyno@users.noreply.github.com>
2026-06-23 10:18:53 +0000

    finish end multis

--- kubejs/startup_scripts/machines/multiblocks/very_late_game_multis/end_multis/true_synthesis_array.js
+++ kubejs/startup_scripts/machines/multiblocks/very_late_game_multis/end_multis/true_synthesis_array.js
@@ -11,7 +11,6 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', (event) => {
 });
 
 GTCEuStartupEvents.registry('gtceu:machine', (event) => {
-    // prettier-ignore
     event
         .create('true_synthesis_array', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
@@ -23,48 +22,47 @@ GTCEuStartupEvents.registry('gtceu:machine', (event) => {
             $StarTRecipeModifiers.THROUGHPUT_BOOSTING,
         ])
         .appearanceBlock(() => Block.getBlock('kubejs:cattomolymer_casing'))
-        .pattern(definition => FactoryBlockPattern.start()
-            .aisle('AABBBAA            ', 'CAADAAC            ', 'C     C            ', 'C     C            ', 'C     C            ', 'C     C            ', 'C     C            ', 'AAAAAAA            ') 
-            .aisle('AAAAAAA            ', 'A  E  A            ', ' AAFAA             ', ' GGGGG             ', ' HIFIH             ', ' GGGGG             ', ' AAFAA             ', 'AAABAAA            ') 
-            .aisle('BAAAAAB            ', 'A  E  A            ', ' A   A             ', ' G E G             ', ' I   I             ', ' G E G             ', ' A   A             ', 'AAFFFAA            ') 
-            .aisle('BAAFAAB            ', 'AEEEEEA            ', ' F E F             ', ' GEEEG             ', ' F E F             ', ' GEEEG             ', ' F E F             ', 'ABFFFBA            ') 
-            .aisle('BAAAAAB            ', 'A  E  A            ', ' A   A             ', ' G E G             ', ' I   I             ', ' G E G             ', ' A   A             ', 'AAFFFAA            ') 
-            .aisle('AAAAAAA            ', 'A  E  A            ', ' A   A             ', ' GGGGG             ', ' HIFIH             ', ' GGGGG             ', ' AAFAA             ', 'AAABAAA            ') 
-            .aisle('AAAFAAAABBBAAABBBAA', 'CAEEEEAAAAAACAAAAAC', 'CAAAAACC   CC     C', 'C     C     C     C', 'C     C     C     C', 'C     C     C     C', 'C     CC   CC     C', 'AAAAAAAAAAAAAAAAAAA') 
-            .aisle('AAAAAAAAAAAAAAAAAAA', 'A  E  E  E  A  E  A', 'CAAFAAAAAFAAAAAFAA ', '        C C  GGGGG ', '         C   HIFIH ', '        C C  GGGGG ', 'C            AAFAA ', 'A     A     AAABAAA') 
-            .aisle('BAAAAAAAAAAAAAAAAAB', 'A  E  E  E  E  E  A', ' AABAAAAAEAAA    A ', ' C           G E G ', '             I   I ', ' C           G E G ', '             A   A ', 'A     A     AAFFFAA') 
-            .aisle('BAAFAAFAAFAAFAAFAAB', 'AEEEEEEEEEEEEEEEEEJ', ' FEBEFAFBBBFA  E F ', '             GEEEG ', ' C           F E F ', '             GEEEG ', '             F E F ', 'A     A     ABFFFBA') 
-            .aisle('BAAAAAAAAAAAAAAAAAB', 'A  E  E  E  E EE  A', ' AABAAAAAEAAA E  A ', ' C           G E G ', '             I   I ', ' C           G E G ', '             A   A ', 'A     A     AAFFFAA') 
-            .aisle('AAAAAAAAAAAAAAAAAAA', 'A  E  E  E  A  E  A', 'CAAFAAAAAFAAAAAFAA ', '        C C  GGGGG ', '         C   HIFIH ', '        C C  GGGGG ', 'C            AAFAA ', 'A     A     AAABAAA') 
-            .aisle('AAAFAAAABBBAAABBBAA', 'CAEEEEAAA@AACAAAAAC', 'CAAAAACC   CC     C', 'C     C     C     C', 'C     C     C     C', 'C     C     C     C', 'C     CC   CC     C', 'AAAAAAAAAAAAAAAAAAA') 
-            .aisle('AAAAAAA            ', 'A  E  A            ', ' A   A             ', ' GGGGG             ', ' HIFIH             ', ' GGGGG             ', ' AAFAA             ', 'AAABAAA            ') 
-            .aisle('BAAAAAB            ', 'A  E  A            ', ' A   A             ', ' G E G             ', ' I   I             ', ' G E G             ', ' A   A             ', 'AAFFFAA            ') 
-            .aisle('BAAFAAB            ', 'AEEEEEA            ', ' F E F             ', ' GEEEG             ', ' F E F             ', ' GEEEG             ', ' F E F             ', 'ABFFFBA            ') 
-            .aisle('BAAAAAB            ', 'A  E  A            ', ' A   A             ', ' G E G             ', ' I   I             ', ' G E G             ', ' A   A             ', 'AAFFFAA            ') 
-            .aisle('AAAAAAA            ', 'A  E  A            ', ' AAFAA             ', ' GGGGG             ', ' HIFIH             ', ' GGGGG             ', ' AAFAA             ', 'AAABAAA            ') 
-            .aisle('AABBBAA            ', 'CAADAAC            ', 'C     C            ', 'C     C            ', 'C     C            ', 'C     C            ', 'C     C            ', 'AAAAAAA            ') 
-            .where('A', P.kjsBlock('cattomolymer_casing')
-                        .or(P.ability(PA.maintenance, {exact:1}))
-                        .or(P.ability(PA.euIn).setMaxGlobalLimited(2))
-                        .or(P.ability(PA.parallelHatch, {max:1}))
-                )
-                .where('B', P.kjsBlock('nyanium_heat_escape_casing'))
-                .where(' ', P.any())
-                .where('C', P.gtBlock('draconyallium_frame'))
-                .where(
-                    'D',
-                    P.ability(PA.fluidIn).or(P.ability(PA.itemIn))
-                )
-                .where('E', P.kjsBlock('nyanium_pipe_casing'))
-                .where('F', P.kjsBlock('nyanium_engine_intake_casing'))
-                .where('G', P.kjsBlock('rhenotax_coil'))
-                .where('H', P.kjsBlock('nyanium_machine_casing'))
-                .where('I', P.kjsBlock('nyanium_firebox_casing'))
-                .where(
-                    'J',
-                    P.ability(PA.itemOut).or(P.ability(PA.fluidOut))
-                )
-                .where('@', P.controller(definition))
+        .pattern((definition) =>
+            newFactoryBlockPattern([
+                'AABBBAA            |CAADAAC            |C     C            |C     C            |C     C            |C     C            |C     C            |AAAAAAA            ',
+                'AAAAAAA            |A  E  A            | AAFAA             | GGGGG             | HIFIH             | GGGGG             | AAFAA             |AAABAAA            ',
+                'BAAAAAB            |A  E  A            | A   A             | G E G             | I   I             | G E G             | A   A             |AAFFFAA            ',
+                'BAAFAAB            |AEEEEEA            | F E F             | GEEEG             | F E F             | GEEEG             | F E F             |ABFFFBA            ',
+                'BAAAAAB            |A  E  A            | A   A             | G E G             | I   I             | G E G             | A   A             |AAFFFAA            ',
+                'AAAAAAA            |A  E  A            | A   A             | GGGGG             | HIFIH             | GGGGG             | AAFAA             |AAABAAA            ',
+                'AAAFAAAABBBAAABBBAA|CAEEEEAAAAAACAAAAAC|CAAAAACC   CC     C|C     C     C     C|C     C     C     C|C     C     C     C|C     CC   CC     C|AAAAAAAAAAAAAAAAAAA',
+                'AAAAAAAAAAAAAAAAAAA|A  E  E  E  A  E  A|CAAFAAAAAFAAAAAFAA |        C C  GGGGG |         C   HIFIH |        C C  GGGGG |C            AAFAA |A     A     AAABAAA',
+                'BAAAAAAAAAAAAAAAAAB|A  E  E  E  E  E  A| AABAAAAAEAAA    A | C           G E G |             I   I | C           G E G |             A   A |A     A     AAFFFAA',
+                'BAAFAAFAAFAAFAAFAAB|AEEEEEEEEEEEEEEEEEJ| FEBEFAFBBBFA  E F |             GEEEG | C           F E F |             GEEEG |             F E F |A     A     ABFFFBA',
+                'BAAAAAAAAAAAAAAAAAB|A  E  E  E  E EE  A| AABAAAAAEAAA E  A | C           G E G |             I   I | C           G E G |             A   A |A     A     AAFFFAA',
+                'AAAAAAAAAAAAAAAAAAA|A  E  E  E  A  E  A|CAAFAAAAAFAAAAAFAA |        C C  GGGGG |         C   HIFIH |        C C  GGGGG |C            AAFAA |A     A     AAABAAA',
+                'AAAFAAAABBBAAABBBAA|CAEEEEAAA@AACAAAAAC|CAAAAACC   CC     C|C     C     C     C|C     C     C     C|C     C     C     C|C     CC   CC     C|AAAAAAAAAAAAAAAAAAA',
+                'AAAAAAA            |A  E  A            | A   A             | GGGGG             | HIFIH             | GGGGG             | AAFAA             |AAABAAA            ',
+                'BAAAAAB            |A  E  A            | A   A             | G E G             | I   I             | G E G             | A   A             |AAFFFAA            ',
+                'BAAFAAB            |AEEEEEA            | F E F             | GEEEG             | F E F             | GEEEG             | F E F             |ABFFFBA            ',
+                'BAAAAAB            |A  E  A            | A   A             | G E G             | I   I             | G E G             | A   A             |AAFFFAA            ',
+                'AAAAAAA            |A  E  A            | AAFAA             | GGGGG             | HIFIH             | GGGGG             | AAFAA             |AAABAAA            ',
+                'AABBBAA            |CAADAAC            |C     C            |C     C            |C     C            |C     C            |C     C            |AAAAAAA            ',
+            ])
+                .whereDict({
+                    A: P.anyOf([
+                        P.kjsBlock('cattomolymer_casing'),
+                        P.ability(PA.maintenance, { exact: 1 }),
+                        P.ability(PA.euIn).setMaxGlobalLimited(2),
+                        P.ability(PA.parallelHatch, { max: 1 }),
+                    ]),
+                    B: P.kjsBlock('nyanium_heat_escape_casing'),
+                    ' ': P.any(),
+                    C: P.gtBlock('draconyallium_frame'),
+                    D: P.abilityOr(PA.fluidIn, PA.itemIn),
+                    E: P.kjsBlock('nyanium_pipe_casing'),
+                    F: P.kjsBlock('nyanium_engine_intake_casing'),
+                    G: P.kjsBlock('rhenotax_coil'),
+                    H: P.kjsBlock('nyanium_machine_casing'),
+                    I: P.kjsBlock('nyanium_firebox_casing'),
+                    J: P.abilityOr(PA.itemOut, PA.fluidOut),
+                    '@': P.controller(definition),
+                })
                 .build()
         )
         .workableCasingModel(
