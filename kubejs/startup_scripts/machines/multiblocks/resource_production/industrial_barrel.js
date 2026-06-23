33171751a1e755d3bb1aa88a95379afa57f777f6
n1xx1 <me@simonemiraglia.it>
2026-06-23 12:10:30 +0200

    fix other typo with multiblock creation recipes

--- kubejs/startup_scripts/machines/multiblocks/resource_production/industrial_barrel.js
+++ kubejs/startup_scripts/machines/multiblocks/resource_production/industrial_barrel.js
@@ -20,7 +20,7 @@ GTCEuStartupEvents.registry('gtceu:machine', (event) => {
     event
         .create('industrial_barrel', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
-        .recipeTypes(['industrial_barrel_aqueous|industrial_barrel_magmatic'])
+        .recipeTypes(['industrial_barrel_aqueous', 'industrial_barrel_magmatic'])
         .recipeModifiers([GTRecipeModifiers.OC_NON_PERFECT_SUBTICK, GTRecipeModifiers.BATCH_MODE])
         .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
         .pattern((definition) =>
