d3f518451f1f3c2d29a69236e1dcaba46a70881a
trulyno <78795220+trulyno@users.noreply.github.com>
2026-07-22 13:06:41 +0200

    sap and resin usages

--- kubejs/server_scripts/additions/multiblock_recipes/large_arboreal_extractor.js
+++ kubejs/server_scripts/additions/multiblock_recipes/large_arboreal_extractor.js
@@ -1,4 +1,6 @@
 ServerEvents.recipes((event) => {
+    const id = global.id;
+
     // Controller Block
     event
         .shaped('start_core:large_arboreal_extractor', ['RSR', 'PGP', 'BTB'], {
@@ -10,4 +12,26 @@ ServerEvents.recipes((event) => {
             T: 'thermal:redstone_servo',
         })
         .id('start:shaped/large_arboreal_extractor');
+
+    event.recipes.gtceu
+        .barrel(id('resin_to_glue'))
+        .itemInputs('gtceu:charcoal_dust')
+        .inputFluids('thermal:resin 500')
+        .outputFluids('gtceu:glue 200')
+        .duration(200);
+
+    event.recipes.gtceu
+        .mixer(id('resin_to_glue'))
+        .itemInputs('#forge:dusts/coal') // coal, charcoal, carbon
+        .inputFluids('thermal:resin 500')
+        .outputFluids('gtceu:glue 500')
+        .duration(200)
+        .EUt(GTValues.VHA[GTValues.LV]);
+
+    event.recipes.gtceu
+        .centrifuge(id('sap_to_sugar'))
+        .inputFluids('thermal:sap 1000')
+        .itemOutputs('8x minecraft:sugar')
+        .duration(350)
+        .EUt(GTValues.VHA[GTValues.LV]);
 });
