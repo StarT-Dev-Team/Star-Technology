b64060d67175994eedaed51b4e855a463bf84a04
UltraPuPower1 <ultrapupower1@gmail.com>
2026-07-22 21:30:29 +0200

    hunt for the error pt.1

--- kubejs/startup_scripts/machines/single_blocks/pulverizer.js
+++ kubejs/startup_scripts/machines/single_blocks/pulverizer.js
@@ -1,17 +1,17 @@
-GTCEuStartupEvents.registry('gtceu:recipe_type', (event) => {
-    event
-        .create('pulverizer')
-        .category('pulverizer')
-        .setEUIO('in')
-        .setMaxIOSize(2, 2, 0, 0)
-        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
-        .setSound(GTSoundEntries.MACERATOR)
-        .setIconSupplier(() => Item.of('gtceu:lv_pulverizer'));
-});
+// GTCEuStartupEvents.registry('gtceu:recipe_type', (event) => {
+//     event
+//         .create('pulverizer')
+//         .category('pulverizer')
+//         .setEUIO('in')
+//         .setMaxIOSize(2, 2, 0, 0)
+//         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+//         .setSound(GTSoundEntries.MACERATOR)
+//         .setIconSupplier(() => Item.of('gtceu:lv_pulverizer'));
+// });
 
-GTCEuStartupEvents.registry('gtceu:recipe_category', (event) => {
-    event.create('pulverizer_heated').recipeType('pulverizer');
-});
+// GTCEuStartupEvents.registry('gtceu:recipe_category', (event) => {
+//     event.create('pulverizer_heated').recipeType('pulverizer');
+// });
 
 GTCEuStartupEvents.registry('gtceu:machine', (event) => {
     event
