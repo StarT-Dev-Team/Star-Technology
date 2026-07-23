fecffee59efabc5764b19400bb019a7c3c784979
UltraPuPower1 <ultrapupower1@gmail.com>
2026-07-23 09:15:17 +0200

    Revert "hunt for the error pt.1"
    time to get error hunting

--- kubejs/startup_scripts/machines/machine_recipe_type_changes.js
+++ kubejs/startup_scripts/machines/machine_recipe_type_changes.js
@@ -1,6 +1,6 @@
-// GTCEuStartupEvents.registry('gtceu:machine', () => {
-//     GCYMMachines.LARGE_MACERATION_TOWER.setRecipeTypes([
-//         GTRecipeTypes.MACERATOR_RECIPES,
-//         GTRecipeTypes.get('pulverizer'),
-//     ]);
-// });
+GTCEuStartupEvents.registry('gtceu:machine', () => {
+    GCYMMachines.LARGE_MACERATION_TOWER.setRecipeTypes([
+        GTRecipeTypes.MACERATOR_RECIPES,
+        GTRecipeTypes.get('pulverizer'),
+    ]);
+});
