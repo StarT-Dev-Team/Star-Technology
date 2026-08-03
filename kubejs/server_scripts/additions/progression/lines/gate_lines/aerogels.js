969929360438ab616bb056e33c7fc53171245549
trulyno <78795220+trulyno@users.noreply.github.com>
2026-08-03 08:31:50 +0200

    don't use alias

--- kubejs/server_scripts/additions/progression/lines/gate_lines/aerogels.js
+++ kubejs/server_scripts/additions/progression/lines/gate_lines/aerogels.js
@@ -5,35 +5,38 @@ ServerEvents.recipes((event) => {
     event.remove({ output: 'gtceu:aerogel_dust' });
     event.remove({ input: 'gtceu:aerogel_dust' });
 
-    const LCR = event.recipes.gtceu.large_chemical_reactor;
-
-    LCR(id('linoleic_acid_seed'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('linoleic_acid_seed'))
         .inputFluids('gtceu:seed_oil 1000', 'gtceu:steam 3000')
         .outputFluids('gtceu:glycerol 1000', 'gtceu:linoleic_acid 3000')
         .duration(800)
         .EUtVA(EV);
 
-    LCR(id('linoleic_acid_fish'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('linoleic_acid_fish'))
         .inputFluids('gtceu:fish_oil 1000', 'gtceu:steam 3000')
         .outputFluids('gtceu:linoleic_acid 3000', 'gtceu:glycerol 1000')
         .duration(800)
         .EUtVA(EV);
 
-    LCR(id('sodium_linoleate_dust'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('sodium_linoleate_dust'))
         .itemInputs('gtceu:sodium_hydroxide_dust')
         .inputFluids('gtceu:linoleic_acid 1000')
         .itemOutputs('gtceu:sodium_linoleate_dust')
         .duration(840)
         .EUtVA(IV);
 
-    LCR(id('aerogel_solvent_mixture'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('aerogel_solvent_mixture'))
         .itemInputs('gtceu:small_sodium_linoleate_dust')
         .inputFluids('gtceu:ethanol 6000', 'minecraft:water 2000')
         .outputFluids('gtceu:aerogel_solvent_mixture 4000')
         .duration(960)
         .EUtVA(IV);
 
-    LCR(id('silicon_tetrachloride'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('silicon_tetrachloride'))
         .itemInputs('gtceu:silicon_dust')
         .inputFluids('gtceu:chlorine 4000')
         .outputFluids('gtceu:silicon_tetrachloride 1000')
@@ -41,13 +44,15 @@ ServerEvents.recipes((event) => {
         .circuit(0)
         .EUtVA(LuV);
 
-    LCR(id('tetraethyl_orthosilicate'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('tetraethyl_orthosilicate'))
         .inputFluids('gtceu:ethanol 4000', 'gtceu:silicon_tetrachloride 1000')
         .outputFluids('gtceu:tetraethyl_orthosilicate 1000')
         .duration(480)
         .EUtVA(LuV);
 
-    LCR(id('aerogel_precursor_solution'))
+    event.recipes.gtceu
+        .large_chemical_reactor(id('aerogel_precursor_solution'))
         .inputFluids(
             'gtceu:aerogel_solvent_mixture 3000',
             'gtceu:tetraethyl_orthosilicate 1000',
