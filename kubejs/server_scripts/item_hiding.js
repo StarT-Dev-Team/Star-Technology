3ee96ae26c8dffcf7110fc2310f4f3a38b257d88
UltraPuPower1 <ultrapupower1@gmail.com>
2026-08-20 11:53:34 +0200

    add item hiding config back
    (removed vintage from config, less clutter)

--- kubejs/server_scripts/item_hiding.js
+++ kubejs/server_scripts/item_hiding.js
@@ -5,4 +5,15 @@ ServerEvents.tags('item', (event) => {
             event.add('c:hidden_from_recipe_viewers', item.id);
         }
     });
+
+    /** @type {string[]} */
+    let hiddenItems = JSON.parse(JsonIO.readJson('kubejs/config/item_hiding.json'));
+    if (!hiddenItems) {
+        console.warn("Config file doesn't exist or is empty");
+    } else {
+        hiddenItems.forEach((/** @type {string} */ itemId) => {
+            itemId.replace('"', '');
+            event.add('c:hidden_from_recipe_viewers', itemId);
+        });
+    }
 });
