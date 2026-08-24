ef2dd268000ba07ba365d5c6a8ceba2fd0ec3fba
trulyno <78795220+trulyno@users.noreply.github.com>
2026-08-24 11:11:15 +0200

    stargate forming helper

--- kubejs/startup_scripts/machines/multiblocks/csg_helper.js
+++ kubejs/startup_scripts/machines/multiblocks/csg_helper.js
@@ -1,17 +1,85 @@
-// GTCEuStartupEvents.registry('gtceu:machine', event => {
-//     event.create('classic_stargate_display', 'multiblock')
-//         .recipeType('dummy')
-// 		.pattern(definition => FactoryBlockPattern.start()
-//             .aisle('   D   ','       ','       ','       ','       ','       ','       ')
-//             .aisle('       ','       ','       ','       ','       ','       ','       ')
-//             .aisle('   E   ','       ','       ','       ','       ','       ','       ')
-//     		.aisle(' RCBCR ','CR   RC','R     R','R  @  R','C     C','RR   RR',' CRCRC ')
-// 			.where('R', Predicates.blocks('sgjourney:classic_stargate_ring_block'))
-//             .where('C', Predicates.blocks('sgjourney:classic_stargate_chevron_block'))
-// 			.where('B', Predicates.blocks('sgjourney:classic_stargate_base_block'))
-//             .where('E', Predicates.blocks('sgjourney:crystal_interface'))
-//             .where('D', Predicates.blocks('sgjourney:classic_dhd'))
-//             .where('@', Predicates.controller(Predicates.blocks(definition.get())))
-// 			.build())
-//         .workableCasingModel('minecraft:block/smooth_stone', 'gtceu:block/machines/object_holder');
-// });
+GTCEuStartupEvents.registry('gtceu:machine', (event) => {
+    event
+        .create('stargate_display', 'multiblock')
+        .recipeType('dummy')
+        .pattern((definition) =>
+            newFactoryBlockPattern([
+                '   D   |       |       |       |       |       |       ',
+                '       |       |       |       |       |       |       ',
+                '   E   |       |       |       |       |       |       ',
+                ' RCBCR |CR   RC|R     R|R  @  R|C     C|RR   RR| CRCRC ',
+            ])
+                .whereDict({
+                    R: P.anyOf([
+                        P.block('sgjourney:classic_stargate_ring_block'),
+                        P.kjsBlock('ancient_stargate_ring_block'),
+                        P.kjsBlock('draconic_stargate_ring_block'),
+                    ]),
+                    C: P.anyOf([
+                        P.block('sgjourney:classic_stargate_chevron_block'),
+                        P.kjsBlock('ancient_stargate_chevron_block'),
+                        P.kjsBlock('draconic_stargate_chevron_block'),
+                    ]),
+                    B: P.anyOf([
+                        P.block('sgjourney:classic_stargate_base_block'),
+                        P.kjsBlock('ancient_stargate_base_block'),
+                        P.kjsBlock('draconic_stargate_base_block'),
+                    ]),
+                    E: P.anyOf([P.block('sgjourney:crystal_interface')]),
+                    D: P.anyOf([
+                        P.block('sgjourney:classic_dhd'),
+                        P.block('sgjourney:milky_way_dhd'),
+                        P.block('sgjourney:pegasus_dhd'),
+                    ]),
+                    '@': P.controller(definition),
+                })
+                .build()
+        )
+        .shapeInfos((definition) => {
+            let shapeInfo = [];
+
+            /** @typedef {internal.com.gregtechceu.gtceu.api.pattern.MultiblockShapeInfo$ShapeInfoBuilder} ShapeInfoBuilder */
+
+            /** @type ShapeInfoBuilder */
+            let builder = MultiblockShapeInfo.builder()
+                .aisle('   D   ', '       ', '       ', '       ', '       ', '       ', '       ')
+                .aisle('       ', '       ', '       ', '       ', '       ', '       ', '       ')
+                .aisle('   E   ', '       ', '       ', '       ', '       ', '       ', '       ')
+                .aisle(' RCBCR ', 'CR   RC', 'R     R', 'R  @  R', 'C     C', 'RR   RR', ' CRCRC ')
+                .where('@', definition, Direction.NORTH)
+                .where('E', Block.getBlock('sgjourney:crystal_interface'));
+
+            shapeInfo.push(
+                builder
+                    .shallowCopy()
+                    .where('R', Block.getBlock('sgjourney:classic_stargate_ring_block'))
+                    .where('C', Block.getBlock('sgjourney:classic_stargate_chevron_block'))
+                    .where('B', Block.getBlock('sgjourney:classic_stargate_base_block'))
+                    .where('D', Block.getBlock('sgjourney:classic_dhd'))
+                    .build()
+            );
+
+            shapeInfo.push(
+                builder
+                    .shallowCopy()
+                    .where('R', Block.getBlock('kubejs:ancient_stargate_ring_block'))
+                    .where('C', Block.getBlock('kubejs:ancient_stargate_chevron_block'))
+                    .where('B', Block.getBlock('kubejs:ancient_stargate_base_block'))
+                    .where('D', Block.getBlock('sgjourney:milky_way_dhd'))
+                    .build()
+            );
+
+            shapeInfo.push(
+                builder
+                    .shallowCopy()
+                    .where('R', Block.getBlock('kubejs:draconic_stargate_ring_block'))
+                    .where('C', Block.getBlock('kubejs:draconic_stargate_chevron_block'))
+                    .where('B', Block.getBlock('kubejs:draconic_stargate_base_block'))
+                    .where('D', Block.getBlock('sgjourney:pegasus_dhd'))
+                    .build()
+            );
+
+            return shapeInfo;
+        })
+        .workableCasingModel('minecraft:block/smooth_stone', 'gtceu:block/machines/object_holder');
+});
